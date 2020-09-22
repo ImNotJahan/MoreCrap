34c2ab3cc984f876adf075c91b661760c9a390f5
ImNotJahan <hiimjahan@gmail.com>
2020-09-22 15:00:28 -0500

    added stone tools

---
+++ Items/StoneAxe.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MoreCrap.Items
+{
+	public class StoneAxe : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("How this doesn't fall apart is a  mystery.");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 5;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.axe = 7;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 3;
+			item.value = 10000;
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.StoneBlock, 15);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/StonePickaxe.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MoreCrap.Items
+{
+	public class StonePickaxe : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("How this doesn't fall apart is a  mystery.");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 4;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 30;
+			item.useAnimation = 30;
+			item.pick = 40;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 3;
+			item.value = 10000;
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.StoneBlock, 20);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
