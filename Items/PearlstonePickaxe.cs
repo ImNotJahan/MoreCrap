9c28232b1e1310f84f4c598ab87a96d07d667be8
ImNotJahan <hiimjahan@gmail.com>
2020-09-22 16:58:28 -0500

    added pearlstone tools

---
+++ Items/PearlstonePickaxe.cs
@@ -0,0 +1,36 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MoreCrap.Items
+{
+	public class PearlstonePickaxe : ModItem
+	{
+		public override void SetDefaults() {
+			item.damage = 5;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 30;
+			item.useAnimation = 30;
+			item.pick = 40;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 3;
+			item.value = 10000;
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.PearlstoneBlock, 20);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
